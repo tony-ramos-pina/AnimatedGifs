6ffafb8a5f7816d71acd77b58caf3f702f31fbd9
tony-ramos-pina <tony.ramos.pina@gmail.com>
2021-02-26 17:26:23 +0000

    second commit, works 4 now

---
+++ animatedgif.me/app/javascript/stylesheets/tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    theme: {},
+    variants: {},
+    plugins: [],
+  }
+
+
+  
+
+//   module.exports = {
+//     purge: [],
+//     darkMode: false, // or 'media' or 'class'
+//     theme: {
+//       extend: {},
+//     },
+//     variants: {
+//       extend: {},
+//     },
+//     plugins: [],
+//   }
--- animatedgif.me/config/routes.rb
+++ animatedgif.me/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  resources :gifs
+  resources :users, only: [:index, :show]
+
+  get '*tag', to: "gifs#random", as: :random_gif
+
+  
+  root to: "gifs#index"
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
